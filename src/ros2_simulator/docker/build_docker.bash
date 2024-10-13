f138161c96d882d4ea2be2d082d2cb020397d8e5
ebrahimabdelghfar <ebrahimabdelghfar550@gmail.com>
2024-10-13 23:24:18 +0300

    without lidar and edit in bash script

--- src/ros2_simulator/docker/build_docker.bash
+++ src/ros2_simulator/docker/build_docker.bash
@@ -1,3 +1,3 @@
-git clone https://github.com/ebrahimabdelghfar/EVER_Simulator.git -b Simulator/ROS2
+git clone https://github.com/ebrahimabdelghfar/EVER_Simulator.git -b Simulator/ROS2 && docker build -t ros2_simulator .
 
-docker build -t ros2_simulator .
+rm -rf EVER_Simulator
