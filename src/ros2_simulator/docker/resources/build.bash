71f180a2e30e24e46df5e0ed62a34083283b6a93
ebrahimabdelghfar <ebrahimabdelghfar550@gmail.com>
2024-10-14 07:04:10 +0300

    adjust the solver

--- src/ros2_simulator/docker/resources/build.bash
+++ src/ros2_simulator/docker/resources/build.bash
@@ -5,6 +5,6 @@ cd /opt/CoppeliaSim_Edu_V4_5_1_rev4_Ubuntu22_04/
 colcon build --symlink-install
 
 # Run the simulator
-./coppeliaSim.sh /src/ros2_simulator/simulation_enviornment/EverAI_Truck.ttt
+./coppeliaSim.sh /EVER_Simulator/src/ros2_simulator/simulation_enviornment/EverAI_Truck.ttt
 
 
